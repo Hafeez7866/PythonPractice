12d8e13c8f9a12cb3a89d6b4427a71bec4bef883
Hafeez7866 <hafeezkhan0hk@gmail.com>
2025-08-02 22:23:09 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,16 @@
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+    else:
+        print('this is prime number {}'.format(i))
+
+x = (1,2,3,4,5,6,7,8,9)
+print(type(x))
+print(len(x))
+y= [1,2,3,4,5,6,7,8,9]
+print(y[0])
+
+
+
+
